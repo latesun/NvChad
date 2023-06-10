2a48166a91412c54d804de5a4aa75ba3728e060f
Latesun <latesun.lee@gmail.com>
2023-06-10 17:44:57 +0800

    feat(ui): customize gruvbox theme

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,18 @@
+---@type ChadrcConfig
+local M = {}
+M.ui = {
+  theme = "gruvbox",
+  transparency = true,
+  changed_themes = {
+    gruvbox = {
+      base_30 = {
+        grey = "#c0b38a",
+        grey_fg = "#b6a980",
+        grey_fg2 = "#ac9f76",
+        line = "#ded1a8",
+        folder_bg = "#b8bb26",
+      },
+    },
+  },
+}
+return M
